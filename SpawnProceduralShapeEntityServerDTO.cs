7402a2c60510ccd901aa91a8ac8b8a64ee4aa89f
JensOV <jensvossnack@gmail.com>
2019-03-11 20:21:38 +0100

    Added transform variables to SpawnProceduralShapeEntityServerDTO

--- SpawnProceduralShapeEntityServerDTO.cs
+++ SpawnProceduralShapeEntityServerDTO.cs
@@ -7,29 +7,55 @@ using UnityEngine;
 
 namespace UnityMultiplayerDRPlugin.DTOs
 {
+    public class UMVector3
+    {
+        public float x, y, z;
+        public UMVector3(float x, float y , float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
     class SpawnProceduralShapeEntityServerDTO : IDarkRiftSerializable
     {
         public uint ID;
         public ProceduralEntityType type;
+        public UMVector3 position;
+        public UMVector3 rotation;
+        public UMVector3 scale;
         public int NrBuildingPoints; // Note: Value is overwritten with buildingPoints.Length on serialize
-        public Vector3[] buildingPoints;
+        public UMVector3[] buildingPoints;
+        
 
         public void Deserialize(DeserializeEvent e)
         {
             ID = e.Reader.ReadUInt32();
             type = (ProceduralEntityType)e.Reader.ReadInt32();
+
+            position = new UMVector3(e.Reader.ReadSingle(), e.Reader.ReadSingle(), e.Reader.ReadSingle());
+            rotation = new UMVector3(e.Reader.ReadSingle(), e.Reader.ReadSingle(), e.Reader.ReadSingle());
+            scale = new UMVector3(e.Reader.ReadSingle(), e.Reader.ReadSingle(), e.Reader.ReadSingle());
+
             NrBuildingPoints = e.Reader.ReadInt32();
-            buildingPoints = new Vector3[NrBuildingPoints];
+            buildingPoints = new UMVector3[NrBuildingPoints];
             for(int i = 0; i < NrBuildingPoints; i++)
             {
-                buildingPoints[i] = new Vector3(e.Reader.ReadSingle(), e.Reader.ReadSingle(), e.Reader.ReadSingle());
+                buildingPoints[i] = new UMVector3(e.Reader.ReadSingle(), e.Reader.ReadSingle(), e.Reader.ReadSingle());
             }
+
+            
         }
 
         public void Serialize(SerializeEvent e)
         {
             e.Writer.Write(ID);
             e.Writer.Write((int)type);
+
+            e.Writer.Write(position.x); e.Writer.Write(position.y); e.Writer.Write(position.z);
+            e.Writer.Write(rotation.x); e.Writer.Write(rotation.y); e.Writer.Write(rotation.z);
+            e.Writer.Write(scale.x); e.Writer.Write(scale.y); e.Writer.Write(scale.z);
 
             NrBuildingPoints = buildingPoints.Length;
             e.Writer.Write(NrBuildingPoints);
