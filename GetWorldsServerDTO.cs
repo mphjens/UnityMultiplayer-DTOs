4aa4f933a83666872564c46ffb53a493856b50ff
JensOV <jensvossnack@gmail.com>
2019-03-23 17:23:43 +0100

    Added scene name nr of players / max players to world dtos

--- GetWorldsServerDTO.cs
+++ GetWorldsServerDTO.cs
@@ -9,18 +9,27 @@ namespace UnityMultiplayerDRPlugin.DTOs
     class WorldDTO : IDarkRiftSerializable
     {
         public string WorldName;
+        public int NrPlayers;
+        public int MaxNrPlayers;
         public ushort SceneEntityID;
+        public string SceneName;
 
         public void Deserialize(DeserializeEvent e)
         {
             WorldName = e.Reader.ReadString();
+            NrPlayers = e.Reader.ReadInt32();
+            MaxNrPlayers = e.Reader.ReadInt32();
             SceneEntityID = e.Reader.ReadUInt16();
+            SceneName = e.Reader.ReadString();
         }
 
         public void Serialize(SerializeEvent e)
         {
             e.Writer.Write(WorldName);
+            e.Writer.Write(NrPlayers);
+            e.Writer.Write(MaxNrPlayers);
             e.Writer.Write(SceneEntityID);
+            e.Writer.Write(SceneName);
         }
     }
 
