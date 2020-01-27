ae79c51817b7c389d60153db3393b722aec45ad8
JensOV <jensvossnack@gmail.com>
2020-01-27 20:18:08 +0100

    Implemented (de)serializer in UMVector3

--- UMVector3.cs
+++ UMVector3.cs
@@ -1,20 +1,42 @@
 using System;
+using DarkRift;
 
 namespace UnityMultiplayerDRPlugin.DTOs
 {
-	public class UMVector3
-	{
+	public class UMVector3 : DarkRift.IDarkRiftSerializable
+    {
 		public float x;
 
 		public float y;
 
 		public float z;
 
-		public UMVector3(float x, float y, float z)
+        public UMVector3()
+        {
+
+        }
+
+
+        public UMVector3(float x, float y, float z)
 		{
 			this.x = x;
 			this.y = y;
 			this.z = z;
 		}
-	}
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            x = e.Reader.ReadSingle();
+            y = e.Reader.ReadSingle();
+            z = e.Reader.ReadSingle();
+
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(x);
+            e.Writer.Write(y);
+            e.Writer.Write(z);
+        }
+    }
 }
