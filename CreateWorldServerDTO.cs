c69941a9fd6c00a62a9805cbfcda9e05817c4c8b
JensOV <jensvossnack@gmail.com>
2020-04-03 23:14:11 +0200

    Added worlddata to createworldserverdto

--- CreateWorldServerDTO.cs
+++ CreateWorldServerDTO.cs
@@ -6,23 +6,28 @@ namespace UnityMultiplayerDRPlugin.DTOs
 	public class CreateWorldServerDTO : IDarkRiftSerializable
 	{
 		public bool Success;
-
 		public string Message;
+		public WorldDTO WorldData;
 
 		public CreateWorldServerDTO()
 		{
+
 		}
 
 		public void Deserialize(DeserializeEvent e)
 		{
 			this.Success = e.Reader.ReadBoolean();
 			this.Message = e.Reader.ReadString();
+			if(this.Success)
+				this.WorldData = e.Reader.ReadSerializable<WorldDTO>();
 		}
 
 		public void Serialize(SerializeEvent e)
 		{
 			e.Writer.Write(this.Success);
 			e.Writer.Write(this.Message);
+			if(this.Success)
+				e.Writer.Write(this.WorldData);
 		}
 	}
 }
